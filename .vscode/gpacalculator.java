135d093b1090a0fb775dacbb08d6a3a5692eb330
SuhaniSharma072 <suhanisharma072@gmail.com>
2026-03-09 16:56:59 -0500

    Refactor GPA calculator to improve user interaction and input handling

--- .vscode/gpacalculator.java
+++ .vscode/gpacalculator.java
@@ -3,6 +3,33 @@ import java.util.*;
 public class gpacalculator {
 
     public static void main (String[]args) {
-        System.out.println("hihi");
+        Scanner kb= new Scanner(System.in);
+        System.out.println("\t\tWelcome To GPA Calculator!\n\t It is an application that calculates your GPA on 100.0 scale.\n\t It can calculate both weigted and unweigted. ");
+        System.out.println("Instructions: Type the extra weight for each type of class ( i.e. Honors or AP_)");
+        // weightages will be stored in variables
+        System.out.print("How many classes do you have?");
+        int N=kb.nextInt();
+        int APweight = 0;
+        int Honorsweight = 0;
+        //AP classes boolean
+        System.out.println("Are you taking AP classes?");
+        boolean AP=kb.nextBoolean();
+        if(AP==true){
+        System.out.print("What is the percentage of weight for AP classes?");
+        int APweight=kb.nextInt();}
+        //Honors classes boolean
+        System.out.println("Are you taking Honors classes?");
+        boolean Honors=kb.nextBoolean();
+        if(Honors==true){
+        System.out.print("What is the percentage of weight for Honors classes?");
+        int Honorsweight=kb.nextInt();}
+            // start calculating gpa
+        double sumtotal=0;
+
+        for(int i = 1; i <= N; i++){
+      
+        
+        
+        
     }
 }
