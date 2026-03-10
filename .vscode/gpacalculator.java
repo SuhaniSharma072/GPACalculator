1288fc13ca9ed124d0698ecc03fa60a471ceb275
SuhaniSharma072 <suhanisharma072@gmail.com>
2026-03-10 13:41:48 -0500

    Improve user prompts and fix GPA calculation logic

--- .vscode/gpacalculator.java
+++ .vscode/gpacalculator.java
@@ -4,48 +4,53 @@ public class gpacalculator {
 
     public static void main (String[]args) {
         Scanner kb= new Scanner(System.in);
-        System.out.println("\t\tWelcome To GPA Calculator!\n\t It is an application that calculates your GPA on 100.0 scale.\n\t It can calculate both weigted and unweigted. ");
-        System.out.println("Instructions: Type the extra weight for each type of class ( i.e. Honors or AP_)");
+        System.out.println("\t\tWelcome To GPA Calculator!\n\t It is an application that calculates your weighted GPA on 100.0 scale.\t ");
+        System.out.println("\tInstructions: Type the extra weight for each type of class ( i.e. Honors or AP)");
         // weightages will be stored in variables
         System.out.print("How many classes do you have?");
         int N=kb.nextInt();
         int APweight = 0;
         int Honorsweight = 0;
+
         //AP classes boolean
-        System.out.println("Are you taking AP classes?");
+        System.out.println("Are you taking AP classes (true/false)?");
         boolean AP=kb.nextBoolean();
         if(AP==true){
         System.out.print("What is the percentage of weight for AP classes?");
-        int APweight=kb.nextInt();}
+        APweight=kb.nextInt();}
         //Honors classes boolean
-        System.out.println("Are you taking Honors classes?");
+        System.out.println("Are you taking Honors classes (true/false)?");
         boolean Honors=kb.nextBoolean();
         if(Honors==true){
         System.out.print("What is the percentage of weight for Honors classes?");
-        int Honorsweight=kb.nextInt();}
+        Honorsweight=kb.nextInt();}
+
             // start calculating gpa
         double sumtotal=0;
 
         for(int i = 1; i <= N; i++){
              System.out.print("Enter grade for class ");
              int grade=kb.nextInt();
+             kb.nextLine();
              System.out.println("Enter class type: On-level, Honors, AP");
              String classType= kb.nextLine();
-             if (classType="On-level"){
+             if (classType.equals("On-level")){
                 grade=grade;
-             } else if (classType="Honors"){
+             } else if (classType.equals("Honors")){
                 grade=grade+Honorsweight;
-             }else if (classType="AP"){
+             }else if (classType.equals("AP")){
                 grade=grade+APweight;
-             }else{
-                System.out.println("Enter valid type");
              }
             
+             sumtotal += grade;
 
             
         }
+
+        double GPA = sumtotal / N;
+        System.out.printf("Your weighted GPA on a 100.0 scale is: %.2f ",GPA);
       
-        
+   //arrayy
         
         
     }
